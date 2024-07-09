f384a8dff0d88fc64ef62bee00f0314a84612c5a
dani <danifromecuador@gmail.com>
2024-07-09 13:11:09 -0500

    Implement Edit function of CRUD | feat/crud | app/controllers/articles_controller.rb & app/views/articles/ |

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,20 @@
 class ArticlesController < ApplicationController
+  # gets all the articles form the DB
   def index
     @articles = Article.all
   end
 
+  # search for one article on DB given it's ID
   def show
     @article = Article.find(params[:id])
   end
 
+  # this will be used for rendering new form
   def new
     @article = Article.new
   end
 
+  # this actually creates a new article  if there's no errors
   def create
     @article = Article.new(article_params)
 
@@ -21,8 +25,24 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # this will be used for rendering edit form for a given article
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # this actually updates a given article if there's no errors
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
+  # manages the params obtained from the new form
   def article_params
     params.require(:article).permit(:title, :body)
   end
